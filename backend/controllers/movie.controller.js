d9a98b50896dd99e17c88ae4a70006db7fc32472
Khapham <khar22102003@gmail.com>
2024-11-20 10:30:06 +0700

    Add movie trailers and movie details endpoint

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -11,3 +11,30 @@ export async function getTrendingMovie(req, res) {
     res.status(500).json({success: false, message: error.message});
   }
 }
+
+export async function getMovieTrailer(req, res) {
+    const { id } = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`);
+        res.json({success: true, trailers: data.results});
+        
+    } catch (error) {
+        if (error.message.includes("404")) {
+            return res.status(404).send(null);
+        }
+        res.status(500).json({success: false, message: error.message});
+    }
+}
+
+export async function getMovieDetails(req, res) {
+    const {id} = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}?language=en-US`);
+        return res.json({success: true, details: data});
+    } catch (error) {
+        if (error.message.includes("404")) {
+            return res.status(404).send(null);
+        }
+        res.status(500).json({success: false, message: error.message});
+    }
+}
--- backend/routes/movies.route.js
+++ backend/routes/movies.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getTrendingMovie } from '../controllers/movie.controller.js';
+import { getTrendingMovie, getMovieTrailer, getMovieDetails } from '../controllers/movie.controller.js';
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
+router.get("/:id/trailers", getMovieTrailer); 
+router.get("/:id/details", getMovieDetails);
 
 
 
