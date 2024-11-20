b33c2390cec19a9f7efaa25962fc3f4a60b52f6d
Khapham <khar22102003@gmail.com>
2024-11-20 10:46:57 +0700

    Add get similar movies and get movies by category endpoint

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -38,3 +38,26 @@ export async function getMovieDetails(req, res) {
         res.status(500).json({success: false, message: error.message});
     }
 }
+
+export async function getSimilarMovies(req, res) {
+    const {id} = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`);
+        return res.json({success: true, similar: data.results});
+    } catch (error) {
+        if (error.message.includes("404")) {
+            return res.status(404).send(null);
+        }
+        res.status(500).json({success: false, message: error.message});
+    }
+}
+
+export async function getMoviesByCategory(req, res) {
+    const {category} = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`);
+        return res.json({success: true, content: data.results});
+    } catch (error) {
+        res.status(500).json({success: false, message: error.message});
+    }
+}
--- backend/routes/movies.route.js
+++ backend/routes/movies.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getTrendingMovie, getMovieTrailer, getMovieDetails } from '../controllers/movie.controller.js';
+import { getTrendingMovie, getMovieTrailer, getMovieDetails, getSimilarMovies, getMoviesByCategory } from '../controllers/movie.controller.js';
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
 router.get("/:id/trailers", getMovieTrailer); 
 router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getSimilarMovies);
+router.get("/:category", getMoviesByCategory);
 
 
 
