cf1baffaadbfd7b52f56382bfd2eef1cb5ce8140
Khapham <khar22102003@gmail.com>
2024-11-20 10:07:03 +0700

    Add movies route, controller and trending movie endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { ENV_VARS } from "./config/envVars.js";
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movies.route.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
@@ -8,7 +9,7 @@ const app = express();
 app.use(express.json()); // parse req.body
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/movies", movieRoutes);
 
 const PORT = ENV_VARS.PORT || 8000;
 app.listen(PORT, () => {
